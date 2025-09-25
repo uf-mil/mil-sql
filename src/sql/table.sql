11d32a6b42ecf36b6d75d0117d0cad7aa0ae7168
Daniel Parra <daniel27parra@gmail.com>
2025-09-25 14:43:12 -0400

    moved around some parts

---
+++ src/sql/table.sql
@@ -0,0 +1,17 @@
+CREATE table(
+	id		INT, 
+	name		VARCHAR(100), 
+	age		INT, 
+	last_update	DATETIME DEFAULT (CURRENT_TIME()),
+	PRIMARY KEY(id)
+);
+
+--WARNING: Inserts should be done in the scripts NOT IN THE SQL FILE--
+
+INSERT INTO table(id, name, age) 
+VALUES 
+(1, "Robin", 22),
+(2, "Beast Boy", 21),
+(3, "Star Fire", 400),
+(4, "Raven", 20),
+(5, "Cyborg", 21);
