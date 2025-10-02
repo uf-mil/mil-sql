ae7dc45fcee3d448ddc57bb679aaa41e534e86af
willzoo <zhujwill@gmail.com>
2025-10-02 14:59:36 -0400

    Working sql commands execute to a local db name db with user:password postgres:admin

--- src/scripts/app.py
+++ src/scripts/app.py
@@ -1,13 +1,22 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgres://postgres:postgres@db:5432/mydb")
+DATABASE_URL = "postgres://postgres:admin@localhost:5432/postgres"
 
 try:
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
     cur.execute("SELECT version();")
     print("Postgres version:", cur.fetchone())
+
+    # Execute SQL from file
+    with open("../sql/table.sql", "r") as f:
+        sql = f.read()
+    print("Executing SQL from file...")
+    print(sql)
+    cur.execute(sql)
+    conn.commit()
+    print("SQL executed successfully.")
     cur.close()
     conn.close()
 except Exception as e:
--- src/sql/table.sql
+++ src/sql/table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE students(
-	id		INT, 
-	name		VARCHAR(100), 
-	age		INT, 
-	last_update	DATETIME DEFAULT (CURRENT_TIME()),
+	id INT, 
+	name VARCHAR(100), 
+	age INT, 
 	PRIMARY KEY(id)
 );
 
@@ -10,8 +9,8 @@ CREATE TABLE students(
 
 INSERT INTO students(id, name, age) 
 VALUES 
-(1, "Robin", 22),
-(2, "Beast Boy", 21),
-(3, "Star Fire", 400),
-(4, "Raven", 20),
-(5, "Cyborg", 21);
+(1, 'Robin', 22),
+(2, 'Beast Boy', 21),
+(3, 'Star Fire', 400),
+(4, 'Raven', 20),
+(5, 'Cyborg', 21);
