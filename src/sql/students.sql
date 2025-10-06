28bd965018406f8cadf64a8e4a03026956b71add
willzoo <zhujwill@gmail.com>
2025-10-05 23:06:46 -0400

    Edit students sql based on the issue in mil-sql

--- src/sql/students.sql
+++ src/sql/students.sql
@@ -1,9 +1,13 @@
 CREATE TABLE students (
-    student_id SERIAL PRIMARY KEY,
-    student_name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    ufid VARCHAR(20) NOT NULL UNIQUE,
-    leader BOOLEAN DEFAULT FALSE,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    uf_id SERIAL PRIMARY KEY,
+    uf_email VARCHAR(150) NOT NULL UNIQUE,
+    phone_number VARCHAR(50),
     team VARCHAR(50),
-    discord_id VARCHAR(50) NOT NULL UNIQUE
+    discord VARCHAR(150) NOT NULL UNIQUE,
+    github VARCHAR(150) NOT NULL UNIQUE,
+    grad_date DATE,
+    join_date DATE,
+    is_leader BOOLEAN DEFAULT FALSE
 );
