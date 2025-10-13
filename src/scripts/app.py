1a36c29407917b730664f8d52dddabbfbbc1a989
willzoo <zhujwill@gmail.com>
2025-10-12 22:06:28 -0400

    Added insert member script and it executes in app.py

--- src/scripts/app.py
+++ src/scripts/app.py
@@ -1,20 +1,13 @@
+from pathlib import Path
 import os
 import psycopg2
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgres://postgres:postgres@db:5432/mydb")
 
-# Function to execute SQL from a file
-def execute_sql_file(file_path: str):
-    try:        
-        conn = psycopg2.connect(DATABASE_URL)
-        cur = conn.cursor()
-        with open(file_path, 'r') as file:
-            sql = file.read()
-        with cur:
-            cur.execute(sql)
-        conn.commit()            
-    except Exception as e:
-        print(f"Unable to execute SQL file at {file_path}: ", e)
+# Global SQL strings
+insert_member = Path("/app/src/sql/insert_member.sql").read_text()
+members_table = Path("/app/src/sql/tables/members.sql").read_text()
+member_progress_table = Path("/app/src/sql/tables/member_progress.sql").read_text()
 
 try:
     conn = psycopg2.connect(DATABASE_URL)
@@ -22,12 +15,47 @@ try:
     cur.execute("SELECT version();")
     print("Postgres version:", cur.fetchone())
 
-    # Execute SQL files
-    execute_sql_file('/app/src/sql/members.sql')
-    execute_sql_file('/app/src/sql/member_progress.sql')
+    # Drop old tables for testing purposes
+    cur.execute("DROP TABLE IF EXISTS member_progress;")
+    cur.execute("DROP TABLE IF EXISTS members;")
+    conn.commit()
+
+    # Make SQL Tables
+    try:
+        cur.execute(members_table)
+    except Exception as e:
+        print("Make member table failed.", e)
+    else:
+        print("Make member table succeeded.")
+    try:
+        cur.execute(member_progress_table)
+    except Exception as e:
+        print("Make progress table failed.", e)
+    else:
+        print("Make progress table succeeded.")
+
+    # Insert a test member
+    try:
+        cur.execute(insert_member, {
+            "first":  "Albert",
+            "last":   "Gator",
+            "email":  "albert.gator@ufl.edu",
+            "phone":  "352-201-0001",
+            "team":   "Mechanical",
+            "discord":"AlbertDiscord",
+            "github": "AlbertGithub",
+            "grad":   "2026-05-01",
+            "join":   "2024-09-01",
+            "leader": False,
+        })
+    except Exception as e:
+        print("Insert member failed:", e)
+    else:
+        print("Insert member succeeded.")
 
     # Finish up
-    print("Creation of all SQL Tables succeeded or attempted.")
+    print("Connection committing and then closing.")
+    conn.commit()
     cur.close()
     conn.close()
 
--- src/sql/member_progress.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE member_progress (
-    progress_id SERIAL PRIMARY KEY,
-    uf_id INT REFERENCES members(uf_id),
-    report_date DATE NOT NULL,
-    progress_percent INT DEFAULT 0
-);
