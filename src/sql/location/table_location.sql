f4cc67485117a22b69387ee0a4154aad2c69027b
willzoo <zhujwill@gmail.com>
2025-10-26 23:29:47 -0400

    more comments

--- src/sql/location/table_location.sql
+++ src/sql/location/table_location.sql
@@ -1,4 +1,6 @@
 -- TODO: Add encoding for actual position coordinates later
+-- Or alternatively, short instructions on how to find the location in the lab
+-- Or both
 CREATE TABLE locations (
     name    VARCHAR(50) PRIMARY KEY
 );
