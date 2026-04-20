ab340d4d739200a5b9bb0bde268ec91f785ddb11
willzoo <zhujwill@gmail.com>
2026-04-20 13:18:51 -0400

    rename sql to tables

--- src/sql/members/insert_member.sql
+++
@@ -1,3 +0,0 @@
--- Inserts into members table and returns the uf_id of the new member
-INSERT INTO members(first_name,last_name,uf_id,uf_email,phone_number,team,discord,github,grad_date,join_date,is_leader)
-VALUES (%(first)s,%(last)s,%(ufid)s,%(email)s,%(phone)s,%(team)s,%(discord)s,%(github)s,%(grad)s,%(join)s,%(leader)s)
--- src/sql/supply_types/migrate_add_prevent_user_edit.sql
+++
@@ -1,4 +0,0 @@
--- Lock type definition edits to leaders only when set (see supply_types.prevent_user_edit).
-ALTER TABLE supply_types
-  ADD COLUMN prevent_user_edit TINYINT(1) NOT NULL DEFAULT 0
-  AFTER is_unique;
